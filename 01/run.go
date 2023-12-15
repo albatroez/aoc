6257a4d72b4a5e4a28b864cba47d4b24b4747d37
Jakub Zielinski <jakub.zielinski@grandparade.co.uk>
2023-12-16 00:55:29 +0100

    day 1 in go

---
+++ 01/run.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	nums := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+	}
+	fi, err := os.Open("input.txt")
+	check(err)
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	sum := 0
+	scanner := bufio.NewScanner(fi)
+	for scanner.Scan() {
+		var first, last rune
+		found := false
+		line := scanner.Text()
+		for pos, char := range line {
+			for str, val := range nums {
+				if strings.HasPrefix(line[pos:], str) {
+					if !found {
+						first = rune(strconv.Itoa(val)[0])
+						found = true
+					} else {
+						last = rune(strconv.Itoa(val)[0])
+					}
+				}
+			}
+			if unicode.IsDigit(char) {
+				if !found {
+					first = char
+					found = true
+				}
+				last = char
+			}
+		}
+		if val, err := strconv.Atoi(string(first) + string(last)); err == nil {
+			sum += val
+		}
+
+	}
+	fmt.Println(sum)
+}
